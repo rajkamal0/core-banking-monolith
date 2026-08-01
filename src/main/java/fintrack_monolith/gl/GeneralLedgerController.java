f1821d72a8443a58380e80abca663994c9249a40
rajkamal0 <rajkandagatla8@gmail.com>
2026-08-01 22:37:12 +0530

    added createGl, closeGl methods in gl controller

--- src/main/java/fintrack_monolith/gl/GeneralLedgerController.java
+++ src/main/java/fintrack_monolith/gl/GeneralLedgerController.java
@@ -2,8 +2,10 @@ package fintrack_monolith.gl;
 
 import java.math.BigDecimal;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,6 +22,17 @@ public class GeneralLedgerController {
 	@GetMapping("/checkbalance/{glNum}")
 	public BigDecimal fetchBalance(@PathVariable String glNum) {
 		return generalLedgerService.fetchBalance(glNum);
+	}
+	
+	@PostMapping("/create")
+	public GeneralLedger createGl(GeneralLedger GLDetails) {
+		return generalLedgerService.createGl(GLDetails);
+	}
+	
+	@PostMapping("/close/{glNum}")
+	public ResponseEntity<Void> closeGl(@PathVariable String glNum) {
+		generalLedgerService.closeGl(glNum);
+		return ResponseEntity.noContent().build();
 	}
 
 }
